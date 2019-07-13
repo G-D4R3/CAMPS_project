aded1aff2781fb75bee0cd0331dd612e893c23fe
ICEB3AR <45871864+ICEB3AR@users.noreply.github.com>
2019-07-13 23:37:52 +0900

    Add calendar view

--- app/src/main/java/com/example/forstudent/CalendarFragment.java
+++ app/src/main/java/com/example/forstudent/CalendarFragment.java
@@ -1,5 +1,6 @@
 package com.example.forstudent;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -7,12 +8,23 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class CalendarFragment extends Fragment {
 
+    private TextView Dday;
+    private TextView today;
     @Nullable
     @Override
+
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_calendar, container, false);
+        View view = (View) inflater.inflate(R.layout.fragment_calendar,container,false);
+
+        return view;
     }
 }
