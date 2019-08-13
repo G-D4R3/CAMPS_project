13e48fd5a8d0b48236282b47c856643dfd8986c1
ICEB3AR <45871864+ICEB3AR@users.noreply.github.com>
2019-08-13 18:14:07 +0900

    시간표 박스 클래스

--- app/src/main/java/com/example/forstudent/BoxClass/Timetable_Model.java
+++ app/src/main/java/com/example/forstudent/BoxClass/Timetable_Model.java
@@ -1,5 +1,22 @@
 package com.example.forstudent.BoxClass;
 
+
+import java.sql.Time;
+
+import io.objectbox.annotation.Entity;
+import io.objectbox.annotation.Id;
+
+@Entity
 public class Timetable_Model {
+    @Id(assignable = true)
+    long id;
+
+    int hour;
+    int minute;
+    int dayOfWeek;
+
+    String professor;
+    String lectureRoom;
+    String subject;
 
 }
