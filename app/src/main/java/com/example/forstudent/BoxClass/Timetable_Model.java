21f51bcc327635c8d06c517c6b2c34a207c5d9ea
yu1mu <all0fus168@gmail.com>
2019-08-20 23:06:04 +0900

    Merge branch 'master' of /Users/yu1mu/Documents/Study/8-9 with conflicts.

--- app/src/main/java/com/example/forstudent/BoxClass/Timetable_Model.java
+++ app/src/main/java/com/example/forstudent/BoxClass/Timetable_Model.java
@@ -19,4 +19,62 @@ public class Timetable_Model {
     String lectureRoom;
     String subject;
 
+    public Timetable_Model(long id, int hour, int minute, int dayOfweek, String professor, String lectureRoom, String subject){
+        this.id = id;
+        this.hour = hour;
+        this.minute = minute;
+        this.dayOfWeek = dayOfweek;
+        this.professor = professor;
+        this.lectureRoom = lectureRoom;
+        this.subject = subject;
+    }
+
+    public void setHour(int hour){
+        this.hour = hour;
+    }
+
+    public void setMinute(int minute){
+        this.minute = minute;
+    }
+
+    public void setDayOfWeek(int dayOfWeek){
+        this.dayOfWeek = dayOfWeek;
+    }
+
+    public void setProfessor(String professor){
+        this.professor = professor;
+    }
+
+    public void setLectureRoom(String lectureRoom){
+        this.lectureRoom = lectureRoom;
+    }
+
+    public void setSubject(String subject){
+        this.subject = subject;
+
+    }
+
+    public int getHour(){
+        return hour;
+    }
+
+    public int getMinute(){
+        return minute;
+    }
+
+    public int getDayOfWeek() {
+        return dayOfWeek;
+    }
+
+    public String getProfessor(){
+        return professor;
+    }
+
+    public String getLectureRoom(){
+        return lectureRoom;
+    }
+
+    public String getSubject(){
+        return subject;
+    }
 }
