e79ba82f2ba3f6fb44f582e31cf95f2610febc77
yu1mu <all0fus168@gmail.com>
2019-08-21 00:27:50 +0900

    시간표 캡처 버튼 (오류)

--- app/src/main/java/com/example/forstudent/MainActivity.java
+++ app/src/main/java/com/example/forstudent/MainActivity.java
@@ -4,9 +4,12 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.style.ForegroundColorSpan;
@@ -38,6 +41,9 @@ import com.example.forstudent.DataClass.Schedule;
 import com.example.forstudent.DataClass.TestSub;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -460,6 +466,40 @@ public class MainActivity<notesBox> extends AppCompatActivity {
         return instance;
     }
 
+    /**** capture ****/
+    //캡쳐버튼클릭
+    public void mOnCaptureClick(View v){
+        //전체화면
+        View rootView = getWindow().getDecorView();
+
+        File screenShot = ScreenShot(rootView);
+        if(screenShot!=null){
+            //갤러리에 추가
+            sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.fromFile(screenShot)));
+        }
+    }
+
+    //화면 캡쳐하기
+    public File ScreenShot(View view){
+        view.setDrawingCacheEnabled(true);  //화면에 뿌릴때 캐시를 사용하게 한다
+
+        Bitmap screenBitmap = view.getDrawingCache();   //캐시를 비트맵으로 변환
+
+        String filename = "screenshot.png";
+        File file = new File(Environment.getExternalStorageDirectory()+"/Pictures", filename);  //Pictures폴더 screenshot.png 파일
+        FileOutputStream os = null;
+        try{
+            os = new FileOutputStream(file);
+            screenBitmap.compress(Bitmap.CompressFormat.PNG, 90, os);   //비트맵을 PNG파일로 변환
+            os.close();
+        }catch (IOException e){
+            e.printStackTrace();
+            return null;
+        }
+
+        view.setDrawingCacheEnabled(false);
+        return file;
+    }
 
 
 
