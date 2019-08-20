cb0347d9f9207e75d1552e4120c7cf3446e22c3c
ICEB3AR <45871864+ICEB3AR@users.noreply.github.com>
2019-08-20 17:06:40 +0900

    save

--- app/src/main/java/com/example/forstudent/MainActivity.java
+++ app/src/main/java/com/example/forstudent/MainActivity.java
@@ -1,6 +1,6 @@
 package com.example.forstudent;
 
-import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.text.Spannable;
@@ -24,6 +25,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.NotificationCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -128,6 +130,26 @@ public class MainActivity<notesBox> extends AppCompatActivity {
 
                 PendingIntent.FLAG_UPDATE_CURRENT);
 
+        NotificationCompat.Builder notificationBuilder = null;
+        NotificationManager notificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+        {
+            String channelId = "default_channel_id";
+            String channelDescription = "Default Channel";
+            NotificationChannel notificationChannel = notificationManager.getNotificationChannel(channelId);
+            if (notificationChannel == null) {
+                int importance = NotificationManager.IMPORTANCE_HIGH;
+                notificationChannel = new NotificationChannel(channelId, channelDescription, importance);
+                notificationChannel.setLightColor(Color.GREEN);
+                notificationChannel.enableVibration(true);
+                notificationManager.createNotificationChannel(notificationChannel);
+            }
+            notificationBuilder = new NotificationCompat.Builder(this, channelId);
+        } else {
+            notificationBuilder = new NotificationCompat.Builder(this);
+        }
+/*
         Notification.Builder builder = new Notification.Builder(this)
 
                 .setSmallIcon(R.drawable.ic_launcher_background) // 아이콘 설정하지 않으면 오류남
@@ -147,7 +169,7 @@ public class MainActivity<notesBox> extends AppCompatActivity {
         notificationManager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
 
         notificationManager.notify(0, builder.build());
-
+*/
 
         Toast.makeText(getApplicationContext(),"Service 시작",Toast.LENGTH_SHORT).show();
         Intent intent = new Intent(MainActivity.this,PushService.class);
